7144544aa9abefc84f811b39d82db8b0eb0599d6
Shubhank Dubey <insightfulshubh@gmail.com>
2025-03-29 18:06:18 +0530

    Update config.py

--- myShop/config.py
+++ myShop/config.py
@@ -13,7 +13,10 @@ class SetupConfig:
     OPENAPI_SWAGGER_UI_PATH = "/docs"
     OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     
-    SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
+    # SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
+    #SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+    SQLALCHEMY_DATABASE_URI = "postgresql://user:pass@sdff.ap-south-1.rds.amazonaws.com:5432/dbname"
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PROPAGATE_EXCEPTIONS = True
 
